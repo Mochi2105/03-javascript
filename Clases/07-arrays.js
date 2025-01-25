e5bd6649963dee5611fbef039c5e1528a0f76758
carolinachavez2105 <cchavezvillacreses1@gmail.com>
2025-01-25 11:36:07 -0500

    ejercicios de javascript basicos

---
+++ Clases/07-arrays.js
@@ -0,0 +1,69 @@
+let myArray = ["Rojo", "verde", "azul"];
+console.log(myArray);
+
+let arrayMixto = [9, "hola mundo", false, NaN, undefined];
+console.log(arrayMixto);
+
+
+let emptyArray = [];
+console.log(emptyArray);
+
+let myNewArray = [];
+for (let i = 0; i < 10; i++) {
+    myNewArray[i] = i + 1 ; 
+}
+
+console.log[myNewArray[4]];
+
+let array1 = ['a', 'b', 'c'];
+let array2 = ['d', 'f', 'g'];
+let array3 = array1.concat(array2);
+console.log(array3);
+
+let array Join = new Array(100)
+console.log(arrJoin)
+
+let arrayJoin = new Array("Hola", "Mi", "nombre", "es", "Carolina");
+let myString = arrayJoin.join("");
+console.log(myString);
+
+// Tomar el ultimo elemento del array y quitarlo
+let arr = [1,2,3,4,5,6,7,8,9];
+let StringPop = arr1.pop();
+console.log(StringPop);
+console.log(arr1);
+
+// Push insertar un elemento en la ultima posicion del array
+let arr2 = [1,2,3,4,5,6,7,8];
+arr2.push(9);
+console.log(arr2);
+
+// Reversar un array
+arr2.reverse();
+console.log(arr2);
+
+// Ordenar un array 
+arr2.sort();
+console.log(arr2);
+
+// Eliminar el primer elemento del array 
+arr2.shift();
+console.log(arr2);
+
+// Saber un indice de un elemento
+console.log(arr2.indexOf(11));
+
+let position = arr2.indexOf(7); 
+arr2.splice(position);
+console.log(arr2);
+
+arr2.splice(0,0,"1")
+console.log(arr2);
+
+// tarea
+const months = ['Jan', 'March', 'April', 'June', 'July'];
+console.log()
+
+
+// Cortar elementos de un array
+const animals= ['ant', 'bison', 'camel', 'duck', 'elephant' ];
